d5f089995e29f95563a432d3c42c51efb775d5b8
Max Degterev <me@maxdegterev.name>
2013-08-24 18:24:49 +0200

    script v1

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ zepto-winscrolled
 =================
 
 Solution for realtime scroll event on mobile
+
+Usage:
+
+Instead of
+
+  $(window).on('scroll', function(e){});
+
+use:
+
+  $(window).on('winscrolled', function(e){ console.warn(e.data) });
---
+++ zepto.winscrolled.js
@@ -0,0 +1,58 @@
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+
+// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+
+// MIT license
+
+(function() {
+    var lastTime = 0;
+    // var vendors = ['ms', 'moz', 'webkit', 'o'];
+    var vendors = ['webkit'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+/* Author:
+    Max Degterev @suprMax
+*/
+
+;(function($) {
+  var lastScrollPos = 0,
+      $win = $(window),
+      rID = null;
+
+  var checkScrollPosition = function() {
+    if (lastScrollPos !== window.pageYOffset) {
+      lastScrollPos = window.pageYOffset;
+      $win.trigger('winscrolled', lastScrollPos);
+    }
+  };
+
+  var loop = function() {
+    checkScrollPosition();
+    rID = window.requestAnimationFrame(loop);
+  };
+
+  $win.on('touchmove', checkScrollPosition);
+  loop();
+
+}(Zepto));
